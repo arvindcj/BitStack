ba3f4d3737316a686ed9b0fac1817e68af7b1bdb
arvindcj <code@arvindcj.com>
2016-07-28 11:57:56 +0530

    add code source

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,12 +1,15 @@
 #!/usr/bin/env bash
 
+VAGRANT_USER="vagrant"
+CODE_PATH="/vagrant/sourcecode"
+
 apt-get upgrade
 apt-get update
 apt-get install flex bison build-essential csh openjdk-6-jdk libxaw7-dev
-mkdir /usr/class
-chown $USER /usr/class
-cd /usr/class
+mkdir $CODE_PATH
+chown $VAGRANT_USER $CODE_PATH
+cd $CODE_PATH
 wget http://spark-university.s3.amazonaws.com/stanford-compilers/vm/student-dist.tar.gz
 tar -xf student-dist.tar.gz
-ln -s /usr/class/cs143/cool /vagrant/
-echo 'PATH=/usr/class/cs143/cool/bin:$PATH' >> /home/vagrant/.bash_profile 
+rm student-dist.tar.gz
+echo 'PATH=/vagrant/sourcecode/cs143/cool/bin:$PATH' >> /home/vagrant/.bash_profile 
